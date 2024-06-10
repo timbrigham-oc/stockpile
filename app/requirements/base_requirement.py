42b83115fe2ca76b678718a98d2bcaa4c30ba482
Tim Brigham <tbrigham@onecallcm.com>
2024-06-10 08:56:31 -0400

    Define logging for BaseRequirement class

--- app/requirements/base_requirement.py
+++ app/requirements/base_requirement.py
@@ -1,7 +1,18 @@
+import logging
+
 class BaseRequirement:
 
     def __init__(self, requirement_info):
         self.enforcements = requirement_info['enforcements']
+        # Define logging for parsers derived from BaseReqirement. 
+        self.log = logging.getLogger(__name__)
+        self.log.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.log.addHandler(ch)
+        self.log.info('Initializing BaseParser instance with name: %s', __name__ )
 
     def is_valid_relationship(self, used_facts, relationship):
         """
